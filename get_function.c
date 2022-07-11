57a950862abac59cd0321e1f22067b27554d658a
kwabenasapong <kwabenasapong@gmail.com>
2022-07-11 23:48:28 +0000

    Add main.h and get_function.c

---
+++ get_function.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * get_func - find the corresponding function
+ * @s: check array
+ * Return: a function to print
+ */
+int (*get_func(char s))(va_list)
+{
+	sps_t sps[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"d", print_d},
+		{"i", print_i},
+		{NULL, NULL}
+	};
+
+	int i;
+
+	for (i = 0; sps[i].c != NULL; i++)
+	{
+		if (*sps[i].c == s)
+			return (sps[i].f);
+	}
+	return (NULL);
