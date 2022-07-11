14aef4d3901c12ec2c75c02d777df5954c02a9ff
KwameSei <nathanielosei92@gmail.com>
2022-07-11 22:16:13 +0000

    Main files created

---
+++ main.h
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ * struct op - flag / function object
+ *@c: flag
+ *@f: function
+ */
+
+typedef struct sps
+{
+char *c;
+int (*f)(va_list arg);
+} sps_t;
+
+int -print(const char *format, ...);
+int _putchar(char c);
+int print_c(va_list arg);
+int (*get_func(char s))(va_list arg);
+int print_s(va_list arg);
+int print_d(va_list arg);
+int print_i(va_list arg);
