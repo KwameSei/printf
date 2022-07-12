284a93582e540932293d99e4d8ccd50210f022dd
kwabenasapong <kwabenasapong@gmail.com>
2022-07-12 01:12:59 +0000

    Update test.c

--- test/test.c
+++ test/test.c
@@ -1,4 +1,4 @@
-nclude <limits.h>
+#include <limits.h>
 #include <stdio.h>
 #include "main.h"
 
@@ -15,6 +15,6 @@ int len2;
 len = _printf("Let's try to printf a simple sentence.\n");
 len2 = printf("Let's try to printf a simple sentence.\n");
 _printf("Length:[%d, %i]\n", len, len);
-printf("Length:[%d, %i]\n", len2, len2)
+printf("Length:[%d, %i]\n", len2, len2);
 return (0);
 }
